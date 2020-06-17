cedcf436ea78dedd97cbb7fe450f3ea4e0ce40fd
erend <erend@DESKTOP-IDRHEP5>
2020-06-17 22:04:26 +0300

    Ortopedigin alt sınıfları toString metoduyla beraber eklendi.

---
+++ src/model/SilikonTabanlik.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author erend
+ */
+public class SilikonTabanlik extends Ortopedik {
+    
+    public SilikonTabanlik(int id, String isim, int fiyat, int adet, String tur) {
+        super(id, isim, fiyat, adet, tur);
+    }
+    
+    @Override
+    public String toString() {
+        return getId() + "\t" + getIsim() + "\t" + getFiyat() + "\t" + getAdet() + "\t" + getTur();
+    }
+    
+}
